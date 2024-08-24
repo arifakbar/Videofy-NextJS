2a94580fdee73fec425672d11f5d43a7dedaf15f
Arif Akbar <arifakbarkv1@gmail.com>
2024-08-25 00:07:47 +0530

    updated live comment

--- components/section/comments.js
+++ components/section/comments.js
@@ -36,14 +36,16 @@ export default function Comments({ videoId, initialComments }) {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socketServerUrl =
-      process.env.NODE_ENV === "production"
-        ? process.env.NEXT_PUBLIC_SITE_URL
-        : "http://localhost:3001";
+    // const socketServerUrl =
+    //   process.env.NODE_ENV === "production"
+    //     ? process.env.NEXT_PUBLIC_SITE_URL
+    //     : "http://localhost:3001";
 
     // console.log("socketServerUrl: ", socketServerUrl);
 
-    const newSocket = io(socketServerUrl);
+    const newSocket = io("https://carnation-fluffy-chanter.glitch.me");
+
+    // const newSocket = io(socketServerUrl);
     setSocket(newSocket);
 
     newSocket.on("connect", () => {
