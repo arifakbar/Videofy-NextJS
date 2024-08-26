6e9c7c079f277db1059076be82e0a1119cbccb6b
Arif Akbar <arifakbarkv1@gmail.com>
2024-08-26 19:24:21 +0530

    removed github auth

--- components/modal/login-modal.js
+++ components/modal/login-modal.js
@@ -50,20 +50,21 @@ export default function LoginModal() {
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
       <DialogContent className="bg-white dark:text-white text-black p-0 overflow-hidden">
-      {loading ? 
-        <div className="h-[250px] flex items-center justify-center">
-          <SpinLoading />
-        </div> :
-        <>
-        <DialogHeader className="p-6">
-          <DialogTitle className="text-2xl text-center font-bold">
-            Login
-          </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Browse and manage your stacks.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col gap-2 items-center justify-between mb-4">
+        {loading ? (
+          <div className="h-[250px] flex items-center justify-center">
+            <SpinLoading />
+          </div>
+        ) : (
+          <>
+            <DialogHeader className="p-6">
+              <DialogTitle className="text-2xl text-center font-bold">
+                Login
+              </DialogTitle>
+              <DialogDescription className="text-center text-zinc-500">
+                Browse and manage your stacks.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col gap-2 items-center justify-between mb-4">
               <div
                 onClick={(e) => handleGoogleLogin(e)}
                 className="
@@ -72,16 +73,17 @@ export default function LoginModal() {
                 <p>Continue with Google</p>
                 <ArrowRight className="w-4 h-4" />
               </div>
-              <div
+              {/* <div
                 onClick={(e) => handleGitHUBLogin(e)}
                 className="
             flex rounded-md py-[8px] cursor-pointer items-center justify-center gap-1 w-[90%] mx-7 bg-green-500 dark:bg-green-500 text-white dark:text-white hover:bg-green-400 dark:hover:bg-green-400 transition"
               >
                 <p>Continue with GitHub</p>
                 <ArrowRight className="w-4 h-4" />
-              </div>
-        </div></>
-      }
+              </div> */}
+            </div>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
